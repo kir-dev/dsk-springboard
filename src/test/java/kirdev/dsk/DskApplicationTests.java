182c1cd7362a83ef3f373104720f0944fdf98f50
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2026-08-21 09:48:38 +0200

    init

---
+++ src/test/java/kirdev/dsk/DskApplicationTests.java
@@ -0,0 +1,13 @@
+package kirdev.dsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
